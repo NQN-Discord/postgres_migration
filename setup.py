8d47029199ae51af2c344b1aad75343379f08683
Blue <redacted>
2020-06-18 16:30:47 +0100

    Ahhh setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="PostgresMigration",
+    name="postgres_migration",
     version="0.1.0",
     description="Migrate NQN's postgres db",
     author='Blue',
     url="https://nqn.blue/",
-    packages=["postgres_migration"],
+    packages=["."],
     entry_points={
         "dbmigrator": [
             "migrations_directory = main:migrations_directory",
