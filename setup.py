73465f740444cf189bd33a18ea99cf02b670e9d6
Blue <redacted>
2020-06-29 12:48:44 +0100

    Attempt to add migrations to setup?

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Migrate NQN's postgres db",
     author='Blue',
     url="https://nqn.blue/",
-    packages=["."],
+    packages=["postgres_migration"],
     entry_points={
         "dbmigrator": [
             "migrations_directory = main:migrations_directory",
