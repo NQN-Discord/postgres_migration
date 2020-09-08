72a7286ed68b276420a397e62cb99c877c5ca234
Blue <redacted>
2020-09-08 20:16:37 +0100

    Add table for packs

--- postgres_migration/main.py
+++ postgres_migration/main.py
@@ -4,3 +4,4 @@ cur_path = os.path.abspath(os.path.dirname(__file__))
 migrations_directory = f"{cur_path}/sql/migrations"
 
 # dbmigrator --migrations-directory=postgres_migration/sql/migrations --config=development.ini generate user_packs
+# dbmigrator --migrations-directory=/usr/local/lib/python3.6/site-packages/postgres_migration/sql/migrations --db-connection-string="user=postgres password=postgres host=postgres" list
---
+++ postgres_migration/sql/migrations/20200908191208_packs.py
@@ -0,0 +1,15 @@
+def up(cursor):
+    cursor.execute("""
+    create table packs
+    (
+        guild_id bigint not null,
+        pack_name varchar not null,
+        is_public boolean not null
+    );
+    """)
+    cursor.execute("create unique index packs_guild_id_uindex on packs (guild_id);")
+    cursor.execute("alter table packs add constraint packs_pk primary key (guild_id);")
+
+
+def down(cursor):
+    cursor.execute("drop table packs")
