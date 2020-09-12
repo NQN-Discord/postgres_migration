77da1e6a72209353e78de2c9a4f52a671349fd04
Blue <redacted>
2020-09-12 15:25:32 +0100

    Update foreign key migration

--- postgres_migration/sql/migrations/20200912141608_user_pack_foreign_key.py
+++ postgres_migration/sql/migrations/20200912141608_user_pack_foreign_key.py
@@ -1,6 +1,8 @@
 def up(cursor):
     cursor.execute("alter table user_packs add constraint user_packs_packs_guild_id_fk foreign key (guild_id) references packs on delete cascade;")
+    cursor.execute("create unique index packs_pack_name_uindex on packs (pack_name);")
 
 
 def down(cursor):
     cursor.execute("alter table user_packs drop constraint user_packs_packs_guild_id_fk;")
+    cursor.execute("drop index packs_pack_name_uindex;")
