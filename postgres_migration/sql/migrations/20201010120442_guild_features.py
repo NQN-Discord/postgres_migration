babb55246e5058315b41f18e4531dbd938f9810c
Blue <redacted>
2020-10-15 19:48:15 +0100

    Add guild features table

--- postgres_migration/sql/migrations/20201010120442_guild_features.py
+++ postgres_migration/sql/migrations/20201010120442_guild_features.py
@@ -1,18 +1,33 @@
 def up(cursor):
     cursor.execute("CREATE TYPE guild_features_enum AS ENUM ('emote_roles')")
-    cursor.execute(
-        """
+    cursor.execute("""
         create table guild_features
         (
             guild_id bigint not null,
-            feature guild_features_enum not null
+            feature guild_features_enum not null,
+            unique (guild_id, feature)
         );
-        """
-    )
+    """)
     cursor.execute("create index guild_features_feature_index on guild_features (feature)")
     cursor.execute("create index guild_features_guild_id_index on guild_features (guild_id)")
+    cursor.execute("alter table guild_settings rename column announcement_channel to nitro_role")
+
+    cursor.execute("CREATE TYPE premium_last_charge_status_enum AS ENUM ('Paid', 'Declined', 'Deleted', 'Pending', 'Refunded', 'Fraud', 'Other')")
+    cursor.execute("""
+        create table premium_users
+        (
+            patreon_id bigint not null constraint patreon_id_pk primary key,
+            discord_id bigint,
+            lifetime_support_cents int,
+            last_charge_date date not null,
+            last_charge_status premium_last_charge_status_enum not null
+        );
+    """)
 
 
 def down(cursor):
-    cursor.execute("DROP TYPE guild_features_enum AS ENUM ('emote_roles')")
+    cursor.execute("DROP TYPE guild_features_enum CASCADE")
     cursor.execute("DROP TABLE guild_features")
+    cursor.execute("alter table guild_settings rename column nitro_role to announcement_channel")
+    cursor.execute("DROP TYPE premium_last_charge_status_enum CASCADE")
+    cursor.execute("DROP TABLE premium_users")
